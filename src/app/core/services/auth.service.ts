1da2e62ef485243317cf03254744d98e597813f5
bagool <ioan.adrian98@gmail.com>
2021-02-19 06:17:56 +0000

    Adds models & proper auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { IResponseWrapper } from '@shared/models/api';
+import { Observable } from 'rxjs';
 import { UserTypes, IUser } from 'src/app/shared/models/user';
 
 @Injectable({
@@ -6,25 +10,49 @@ import { UserTypes, IUser } from 'src/app/shared/models/user';
 })
 export class AuthService {
 
+  private baseURL: string;
+
   // TODO: remove when backend is implemented
-  private logIns: IUser[] = [
-    {
-      email: 'test@email.com',
-      password: 'password',
-      type: UserTypes.Patient
+  // private logIns: IUser[] = [
+  //   {
+  //     email: 'test@email.com',
+  //     password: 'password',
+  //     type: UserTypes.Patient
+  //   }
+  // ];
+
+  constructor(private httpClient: HttpClient) { 
+    this.baseURL = environment.apiBaseURL;
+  }
+
+  logIn(email: string, password: string): Observable<IResponseWrapper<any>> {
+
+    const params = new HttpParams({
+      fromObject: {
+        email,
+        password
+      }
+    });
+
+    return this.httpClient.get<IResponseWrapper<any>>(`${this.baseURL}/login`, { params });
+  }
+
+  register(user: IUser): Observable<IResponseWrapper<IUser>> {
+
+    let endpoint = '';
+
+    switch (user?.type) {
+      
+      case UserTypes.GP:
+        endpoint = `${this.baseURL}/gps`;
+        break;
+      
+      default:
+        // default to patient since they have the least access
+        endpoint = `${this.baseURL}/patients`;
+        break;
     }
-  ];
 
-  constructor() { }
-
-  logIn(email: string, password: string): IUser | undefined {
-
-    const userData = this.logIns.find(u => u.email === email);
-
-    if (userData?.password === password) {
-      return userData;
-    }
-
-    return undefined;
+    return this.httpClient.post<IResponseWrapper<IUser>>(endpoint, user);
   }
 }
--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,5 +1,8 @@
+import { JsonProperty } from "json-typescript-mapper";
+
 export enum UserTypes {
     Patient = "patient",
+    GP = "gp",
     Pharmacist = "pharmacist",
     Unknown = "na"
 }
@@ -7,5 +10,79 @@ export enum UserTypes {
 export interface IUser {
     email: string;
     password: string;
-    type: UserTypes;
+    firstName: string;
+    lastName: string;
+    type?: UserTypes;
+}
+
+
+export interface IBloodTest {
+    date: string;
+    result: string;
+}
+
+export class Prescription {
+    @JsonProperty('drug-name')
+    drugName!: string;
+
+    dose!: string;
+    instructions!: string;
+
+    @JsonProperty('end-date')
+    endDate!: string;
+
+    @JsonProperty('start-date')
+    startDate!: string;
+
+    @JsonProperty('GP-data')
+    gpData!: string[];
+
+    id!: number;
+}
+
+export class PatientPrescription {
+    
+    @JsonProperty('patient-email')
+    patientEmail!: string;
+
+    @JsonProperty('GP-email')
+    gpEmail!: string;
+
+    @JsonProperty('end-date')
+    endDate!: string;
+
+    @JsonProperty('drug-name')
+    drugName!: string;
+
+    dose!: string;
+    instructions!: string;
+}
+
+export interface IPatientPrescriptions {
+    current: Prescription[],
+    previous: Prescription[],
+}
+
+export class Patient {
+    email!: string;
+    password!: string;
+    firstName?: string;
+    lastName?: string;
+    gps?: string[];
+    bloodTest?: IBloodTest[];
+    prescriptions?: IPatientPrescriptions;
+}
+
+export interface IPrescriptionRequest {
+    pending: string[];
+    accepted: string[];
+    denied: string[];
+}
+
+export class GP {
+    email!: string;
+    firstName!: string;
+    lastName!: string;
+    patients!: string[];
+    prescriptionRequests!: IPrescriptionRequest;
 }
