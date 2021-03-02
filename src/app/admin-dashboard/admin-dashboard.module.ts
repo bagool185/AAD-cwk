a53c45f8566ca04ace77bf39ac690ac34de5de86
bagool <ioan.adrian98@gmail.com>
2021-03-02 20:07:11 +0000

    Removes redundant modals

--- src/app/admin-dashboard/admin-dashboard.module.ts
+++ src/app/admin-dashboard/admin-dashboard.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AdminDashboardRoutingModule } from './admin-dashboard-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { SharedModule } from '@shared/shared.module';
-import { ConfirmUserDeletionModalComponent } from './admin-dashboard/confirm-user-deletion-modal/confirm-user-deletion-modal.component';
 
 
 @NgModule({
-  declarations: [AdminDashboardComponent, ConfirmUserDeletionModalComponent],
+  declarations: [AdminDashboardComponent],
   imports: [
     CommonModule,
     AdminDashboardRoutingModule,
--- src/app/admin-dashboard/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -5,7 +5,6 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ConfirmModalComponent } from '@shared/confirm-modal/confirm-modal.component';
 import { IUser, UserTypes } from '@shared/models/user';
-import { ConfirmUserDeletionModalComponent } from './confirm-user-deletion-modal/confirm-user-deletion-modal.component';
 
 @Component({
   selector: 'app-admin-dashboard',
