d295639cea0a92f3f364ee6b3c5f6eb8af053bac
bagool <ioan.adrian98@gmail.com>
2021-03-05 12:53:42 +0000

    potential fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseURL: 'http://ec2-18-216-86-26.us-east-2.compute.amazonaws.com:5000'
+  apiBaseURL: 'http://ec2-18-216-86-26.us-east-2.compute.amazonaws.com:5000/'
 };
