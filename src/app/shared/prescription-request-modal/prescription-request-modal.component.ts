9b9eed292f9837dfeff0cd3bb2f3a48870e08911
bagool <ioan.adrian98@gmail.com>
2021-03-05 11:56:32 +0000

    Adds blood results to modals

--- src/app/shared/prescription-request-modal/prescription-request-modal.component.ts
+++ src/app/shared/prescription-request-modal/prescription-request-modal.component.ts
@@ -1,5 +1,8 @@
+import { RepositionScrollStrategy } from '@angular/cdk/overlay';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DrugsService } from '@services/drugs.service';
+import { IDrug } from '@shared/models/medication';
 import { IPrescriptionRequest } from '@shared/models/prescriptions';
 
 @Component({
@@ -10,14 +13,23 @@ import { IPrescriptionRequest } from '@shared/models/prescriptions';
 export class PrescriptionRequestModalComponent implements OnInit {
 
   prescriptionRequest!: IPrescriptionRequest;  
+  drugData: IDrug | undefined; 
 
   constructor(
-    public dialogRef: MatDialogRef<PrescriptionRequestModalComponent>,
+    private readonly dialogRef: MatDialogRef<PrescriptionRequestModalComponent>,
+    private readonly drugsService: DrugsService,
      @Inject(MAT_DIALOG_DATA) private readonly data: { prescriptionRequest: IPrescriptionRequest }) { 
   }
 
   ngOnInit(): void {
     this.prescriptionRequest = this.data.prescriptionRequest;
+  
+    this.drugsService.getAll().subscribe((res) => {
+      
+      this.drugData = res.data.find(d => d.drugName === this.prescriptionRequest.drugName);
+      console.log(this.drugData?.bloodsRequired[0].result);
+      console.log(this.drugData?.bloodsRequired)
+    });
   }
 
   close() {
